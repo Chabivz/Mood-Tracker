ea5a05ae953ad1b23fbfe8a2017c7d26e13bc707
Chris Abiva <chrisabiva@hotmail.com>
2021-05-01 12:57:15 -0700

    Calm Quote Funtion

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,3 +7,35 @@ function displayDate() {
 }
 
 displayDate();
+
+
+// Calm API
+const myURL = 'https://favqs.com/api/qotd?tags=love';
+const calmQuoteEl = document.querySelector("#quoteDiv");
+const calmAuthorEl = document.querySelector("#authorDiv");
+const calmButtonEl = document.querySelector("#calm-button");
+
+function getCalmApi() {
+  fetch(myURL)
+    .then(function (response) {
+      console.log(response.status);
+      //  Conditional for the the response.status.
+      if (response.status !== 200) {
+        // Place the response.status on the page.
+        // responseTextEL.textContent = response.status; 
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      // Make sure to look at the response in the console and read how 404 response is structured.
+      console.log(data);
+      const bodyQuote = data.quote.body;
+      const author = data.quote.author;
+      console.log(data.quote.body);
+      console.log(data.quote.author);
+      calmAuthorEl.textContent = calmQuoteEl;
+      calmAuthorEl.textContent = calmAuthorEl;
+    });
+}
+
+calmButtonEl.onclick = getCalmApi;
